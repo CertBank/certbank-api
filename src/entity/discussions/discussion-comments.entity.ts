083039f6ef61579fc6d577f49894c0a95e679352
JH8459 <kk_ong2233@naver.com>
2025-06-06 22:06:08 +0900

    Feature: 엔티티 추가 (DiscussionCommentsEntity)

---
+++ src/entity/discussions/discussion-comments.entity.ts
@@ -0,0 +1,39 @@
+import { Entity, Index, ManyToOne, Property } from '@mikro-orm/core';
+import { SoftDeletableEntity } from '../base.entity';
+import { UsersEntity } from '../users/users.entity';
+import { DiscussionsEntity } from './discussions.entity';
+
+/**
+ * 토의 댓글 엔티티
+ * @description 토의에 달리는 댓글을 관리하는 엔티티입니다.
+ * 대댓글은 지원하지 않으며, 토의에 직접 달리는 댓글만 허용합니다.
+ * @entity
+ * @table discussion_comments
+ */
+@Entity({ tableName: 'discussion_comments' })
+@Index({ properties: ['discussions', 'createdAt'] })
+export class DiscussionCommentsEntity extends SoftDeletableEntity {
+  /**
+   * 토의 참조
+   * @description 댓글이 달린 토의를 참조합니다.
+   * @type {DiscussionsEntity}
+   */
+  @ManyToOne(() => DiscussionsEntity)
+  discussions!: DiscussionsEntity;
+
+  /**
+   * 작성자 참조
+   * @description 댓글을 작성한 사용자를 참조합니다.
+   * @type {UsersEntity}
+   */
+  @ManyToOne(() => UsersEntity)
+  users!: UsersEntity;
+
+  /**
+   * 댓글 내용
+   * @description 댓글의 텍스트 내용입니다.
+   * @type {string}
+   */
+  @Property({ type: 'text', fieldName: 'content' })
+  content!: string;
+}

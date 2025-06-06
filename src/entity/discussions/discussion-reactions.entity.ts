bf7e03231c768132220bcb3af50b5b9adf5bb3da
JH8459 <kk_ong2233@naver.com>
2025-06-06 22:03:27 +0900

    Feature: 엔티티 추가 (DiscussionReactionsEntity)

---
+++ src/entity/discussions/discussion-reactions.entity.ts
@@ -0,0 +1,53 @@
+import { Entity, Enum, Index, ManyToOne, Property } from '@mikro-orm/core';
+import { BaseEntity } from '../base.entity';
+import { UsersEntity } from '../users/users.entity';
+import { DiscussionsEntity } from './discussions.entity';
+import { ReactionTypeEnum } from '../../common/constant/enum';
+import { DiscussionCommentEntity } from './discussion-comments.entity';
+
+/**
+ * 토의 반응 엔티티
+ * @description 사용자의 토의에 대한 반응(좋아요/싫어요)을 관리하는 엔티티입니다.
+ * 한 사용자는 하나의 토의에 대해 하나의 반응만 가질 수 있습니다.
+ * @entity
+ * @table discussion_reactions
+ */
+@Entity({ tableName: 'discussion_reactions' })
+@Index({ properties: ['discussions', 'reactionType'] })
+@Index({ properties: ['discussionComments', 'reactionType'] })
+export class DiscussionReactionEntity extends BaseEntity {
+  /**
+   * 토의 참조 (선택적)
+   * @description 반응이 달린 토의를 참조합니다.
+   * @type {DiscussionsEntity}
+   * @nullable true
+   */
+  @ManyToOne(() => DiscussionsEntity, { nullable: true })
+  discussions?: DiscussionsEntity;
+
+  /**
+   * 댓글 참조 (선택적)
+   * @description 반응이 달린 댓글을 참조합니다.
+   * @type {DiscussionCommentEntity}
+   * @nullable true
+   */
+  @ManyToOne(() => DiscussionCommentEntity, { nullable: true })
+  discussionComments?: DiscussionCommentEntity;
+
+  /**
+   * 사용자 참조
+   * @description 반응을 남긴 사용자를 참조합니다.
+   * @type {UsersEntity}
+   */
+  @ManyToOne(() => UsersEntity)
+  users!: UsersEntity;
+
+  /**
+   * 반응 타입
+   * @description 좋아요 또는 싫어요 타입입니다.
+   * @type {ReactionTypeEnum}
+   */
+  @Enum(() => ReactionTypeEnum)
+  @Property({ type: 'enum', fieldName: 'reaction_type' })
+  reactionType!: ReactionTypeEnum;
+}

3d77af15e374c78715ddaf443877d3d5d73e8c68
JH8459 <kk_ong2233@naver.com>
2025-06-10 09:07:05 +0900

    Modify: seeder 수정 (questions)

--- src/database/seeders/questions/questions.seeder.ts
+++ src/database/seeders/questions/questions.seeder.ts
@@ -5,6 +5,10 @@ import { QuestionSectionsEntity } from '../../../entities/questions/question-sec
 import { QuestionSetsEntity } from '../../../entities/questions/question-sets.entity';
 import { YNEnum } from '../../../common/constants/enum';
 
+// 🎯 QuestionsEntity에서 자동으로 DTO 생성
+type QuestionEntityFields = EntityToSeed<QuestionsEntity>;
+interface QuestionSeedDto extends ReadonlySeed<QuestionEntityFields> {}
+
 export class QuestionsSeeder extends Seeder {
   async run(em: EntityManager): Promise<void> {
     console.log('🌱 Questions 시딩 시작...');
@@ -14,24 +18,39 @@ export class QuestionsSeeder extends Seeder {
     console.log(`🗑️ ${deleteCount}개 Question 삭제 완료`);
 
     // 🔥 QuestionSet을 이름으로 미리 조회
+    const setTitle = 'SQLD';
+
     const sqld = await em.findOne(QuestionSetsEntity, {
-      setTitle: 'SQLD',
+      setTitle,
     });
+
+    if (!sqld) {
+      throw new Error(`${setTitle} QuestionSet을 찾을 수 없습니다. QuestionSetsSeeder를 먼저 실행해주세요.`);
+    }
 
     // 🔥 QuestionSection을 이름으로 미리 조회
+    const sectionTitle = '데이터 모델링의 이해';
+
     const sqld_1 = await em.findOne(QuestionSectionsEntity, {
-      sectionTitle: '데이터 모델링의 이해',
+      sectionTitle,
     });
 
+    if (!sqld_1) {
+      throw new Error(
+        `${sectionTitle} QuestionSection을 찾을 수 없습니다. QuestionSectionsSeeder를 먼저 실행해주세요.`,
+      );
+    }
+
     // 새 데이터 삽입
-    const questions: any[] = [
+    const questions: QuestionSeedDto[] = [
       {
+        questionSet: sqld,
+        questionSection: sqld_1,
         no: 1,
         content: '<p>데이터 모델링의 특징으로 가장 적절하지 <u>않은</u> 것은?</p>',
         subContentUseYn: YNEnum.NO,
         multipleOptionsUseYn: YNEnum.NO,
-        questionSet: sqld,
-        questionSection: sqld_1,
+        multipleOptionCnt: 1,
       },
     ];
 
